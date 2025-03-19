b269a40239ae8183328630c00d28fbdc93160adf
3Matvey <andrianov8m@gmail.com>
2025-03-19 16:13:07 +0300

    main.tsx fix

--- eventmaster.client/src/main.tsx
+++ eventmaster.client/src/main.tsx
@@ -2,32 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store'
-import { updateServerHostAddress } from './services/constants/Server_HostAddress'
+import { Provider } from 'react-redux';
+import { store } from './store';
 
-async function initApp() {
-    try {
-        const response = await fetch('/config.json')
-        if (!response.ok) {
-            throw new Error('Failed to load config.json')
-        }
-        const config = await response.json()
-        // Обновляем нашу переменную Server_HostAddress
-        updateServerHostAddress(config.SERVER_HOST_ADDRESS)
-    } catch (error) {
-        console.error('Error loading config.json:', error)
-        // Можно установить значение по умолчанию, если конфигурация не загрузилась
-        updateServerHostAddress('http://localhost:5242')
-    }
-
-    createRoot(document.getElementById('root')!).render(
-        <Provider store={store}>
-            <StrictMode>
-                <App />
-            </StrictMode>
-        </Provider>,
-    )
-}
-
-initApp()
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </Provider>,
+)
