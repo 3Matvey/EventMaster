b94c8dd273348efdf827475cdaf6b7954302b70a
3Matvey <andrianov8m@gmail.com>
2025-03-20 13:14:16 +0300

    mbfixx1

--- eventmaster.client/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Replacing environment variables in config.template.json..."
-envsubst < /usr/share/nginx/html/config.template.json > /usr/share/nginx/html/config.json
-
-echo "Starting Nginx..."
-exec nginx -g 'daemon off;'
